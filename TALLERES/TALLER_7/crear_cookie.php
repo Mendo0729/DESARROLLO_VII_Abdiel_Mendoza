14ab948ba28dcf89a8652092791f607206915f08
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2025-10-13 18:56:57 -0500

    TALLER_7 - Paso 1: Implementación básica de cookies

--- TALLERES/TALLER_7/crear_cookie.php
+++ TALLERES/TALLER_7/crear_cookie.php
@@ -1,7 +1,18 @@
 <?php
 // Crear una cookie que expira en 1 hora
-setcookie("usuario", "Juan", time() + 3600, "/");
+//setcookie("usuario", "Juan", time() + 3600, "/");
 
-echo "Cookie 'usuario' creada.";
+//echo "Cookie 'usuario' creada.";
+
+setcookie("usuario", "Juan", [
+    'expires' => time() + 3600,
+    'path' => '/',
+    'domain' => '',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
+
+echo "Cookie segura 'usuario' creada.";
 ?>
      
